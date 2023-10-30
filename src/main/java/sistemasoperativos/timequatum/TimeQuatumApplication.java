58fcbd3cfaddc806bbfbd3d718191624cd7f8ff1
Rakkun9 <jnicolaybv@gmail.com>
2023-10-29 20:45:30 -0500

    Initial commit

---
+++ src/main/java/sistemasoperativos/timequatum/TimeQuatumApplication.java
@@ -0,0 +1,13 @@
+package sistemasoperativos.timequatum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeQuatumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeQuatumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sistemasoperativos/timequatum/TimeQuatumApplicationTests.java
@@ -0,0 +1,13 @@
+package sistemasoperativos.timequatum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeQuatumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
